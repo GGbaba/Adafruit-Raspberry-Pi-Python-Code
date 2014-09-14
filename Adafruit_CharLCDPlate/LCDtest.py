25dd5aebdf068eb7ae62aebd71e6774b3a7eccea
GG <taco_licou@hotmail.com>
2014-09-14 19:35:12 +0200

    Update LCDtest.py

--- Adafruit_CharLCDPlate/LCDtest.py
+++ Adafruit_CharLCDPlate/LCDtest.py
@@ -31,7 +31,6 @@ prev = -1
 while True:
     for b in btn:
         if lcd.buttonPressed(b[0]):
-            call(["ls", "-l"])
             if b is not prev:
                 lcd.clear()
                 lcd.message(b[1])
