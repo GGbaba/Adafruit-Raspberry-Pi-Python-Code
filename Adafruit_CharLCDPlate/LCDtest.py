d0f79ab8240af7ebb8af82b512f229f0d2bb9d77
GG <taco_licou@hotmail.com>
2014-09-15 11:48:48 +0200

    Update LCDtest.py

--- Adafruit_CharLCDPlate/LCDtest.py
+++ Adafruit_CharLCDPlate/LCDtest.py
@@ -40,7 +40,7 @@ a=0
 c=0
 while True:
     for b in btn:
-      sleep(.5) 
+        sleep(.5) 
         if lcd.buttonPressed(b[0]):
                #if lcd.message(b[1]) == "mpc":
                if b[0] == 0 :
